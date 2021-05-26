bcc1a096e5572ca850bcab057b0f562102c47e2d
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-05-26 10:32:34 +0900

    mobile or not mobile indexHandler change

--- main.js
+++ main.js
@@ -2,11 +2,58 @@
 
 // variable
 let currentStatus = 0; // 컴퓨터 디스플레이 0, 모바일 1
-let currentSection = 1; // 현재 섹션
+let currentSection = 0; // 현재 섹션
 let navOffsetTop = null;
 //  DOM
 const mainSection = document.querySelectorAll('.main__section');
 const navbar = document.querySelector('#navbar');
+const navMenu = document.querySelector('.navbar__menu');
+
+
+console.log(navMenu.children);
+for(let menu of navMenu.children) {
+    console.log(menu);
+}
+
+const mobileIndexHandler = (event) => {
+    console.log('here')
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if(!link) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
+const computerIndexHandler = (event) => {
+    console.log('here2');
+    if(!event.target.dataset.index) return;
+    currentSection = parseInt(event.target.dataset.index);
+    for(let section of mainSection) {
+        if(section.id === event.target.dataset.link.slice(1)) {
+            if(section.classList.contains('noShow')) {
+                section.classList.remove('noShow');
+            }
+        } else {
+            if(!section.classList.contains('noShow')) {
+                section.classList.add('noShow');
+            }
+        }
+    }
+}
+
+const indexHandlerChange = (currentStatus) => {
+    if(currentStatus) {
+        navMenu.removeEventListener('click', computerIndexHandler);
+        navMenu.addEventListener('click', mobileIndexHandler);
+    } else {
+        navMenu.removeEventListener('click', mobileIndexHandler);
+        navMenu.addEventListener('click', computerIndexHandler)
+    }
+}
+
 
 // function
 const setIndexHandler = () => {
@@ -45,16 +92,24 @@ const setNavBackground = () => {
 // event handlers
 
 window.addEventListener('load', () => {
+    console.log(window.innerWidth)
+    if(window.innerWidth > 994) {
+        currentStatus = 0
+    } else {
+        currentStatus = 1;
+    }
     setIndexHandler();
     setNavOffsetTop();
+    indexHandlerChange(currentStatus);
 });
 window.addEventListener('scroll', () => {
     setNavBackground();
 })
 
 
-let tmpStatus = null;
+
 window.addEventListener('resize', () => {
+    let tmpStatus;
     if(window.innerWidth > 994) {
         tmpStatus = 0;
     } else {
@@ -62,10 +117,12 @@ window.addEventListener('resize', () => {
     }
 
     if(currentStatus === tmpStatus) {
-        // console.log(currentStatus, tmpStatus);
+        console.log(currentStatus);
     } else {
         setIndexHandler();
         currentStatus = tmpStatus;
+        console.log('currentStatus', currentStatus);
+        indexHandlerChange(currentStatus);
     }
 
     if(currentStatus) {
